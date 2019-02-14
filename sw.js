9bc794b3c7523d072ce5925e97e411af1e11e3f3
Jared Nielsen <nielsen.jared@gmail.com>
2019-02-14 08:31:53 -0500

    Updates

--- sw.js
+++ sw.js
@@ -26,20 +26,20 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-937c89a3ace0f70faf92.js"
+    "url": "webpack-runtime-ca4ca898c8c87cf72ded.js"
   },
   {
     "url": "app.65d321c18066299f7f20.css"
   },
   {
-    "url": "app-3b7d3762021c22adbf7d.js"
+    "url": "app-3339798dd28c162db386.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-4bdd77720b900ef62e34.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4c59cce43f8c67769716c3c8111d0c3d"
+    "revision": "eb954c32fbeaa402c7a7f344d2c6832b"
   },
   {
     "url": "component---src-pages-404-js-944d6afc71ddf57ded68.js"
